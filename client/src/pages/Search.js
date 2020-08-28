ef787a73ce609e8d7f5f20ca5d412f9f39a71649
Jorge Guzman <jorguzman100@gmail.com>
2020-08-28 23:47:01 +0300

    Book Search component created

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 // Components
+import CarouselC from '../components/CarouselC';
+import BookSearch from '../components/BookSearch';
+import Results from '../components/Results';
+
 
 const Search = () => {
-
+    return (
+        <Container>
+            <CarouselC />
+            <BookSearch />
+            <Results />
+        </Container>
+    );
 }
 
 export default Search;
